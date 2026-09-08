9b7156cd94db58dbd59130623adb96a0fc03b973
rockstaraki2-byte <rockstaraki2@gmail.com>
2026-09-08 16:37:50 -0300

    refactor: expose modular FM experience API

---
+++ src/app/fm-experience.ts
@@ -0,0 +1,2 @@
+export { FM_ENTITY_EVENT, FM_GROUPS, FM_MOBILE_PRIMARY, FmContextBar, FmMobileMenu, FmSidebarNav, openFmEntity, type FmEntity } from "./fm-nav";
+export { DataHubDecisionStrip, DressingRoomHierarchy, FmSpecialView, ManagerPortal } from "./fm-surfaces";
