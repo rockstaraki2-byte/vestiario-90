e1e8000051850c4b3a767a2c2267e81ea6b3bd5e
rockstaraki2-byte <rockstaraki2@gmail.com>
2026-09-08 17:14:33 -0300

    feat: expose V90 6 navigation and depth special views

--- src/app/fm-experience.ts
+++ src/app/fm-experience.ts
@@ -1,2 +1,4 @@
-export { FM_ENTITY_EVENT, FM_GROUPS, FM_MOBILE_PRIMARY, FmContextBar, FmMobileMenu, FmSidebarNav, openFmEntity, type FmEntity } from "./fm-nav";
-export { DataHubDecisionStrip, DressingRoomHierarchy, FmSpecialView, ManagerPortal } from "./fm-surfaces";
+export { FM_ENTITY_EVENT, FmContextBar, openFmEntity, type FmEntity } from "./fm-nav";
+export { FM_GROUPS, FM_MOBILE_PRIMARY, FmMobileMenu, FmSidebarNav } from "./fm-v6-nav";
+export { DataHubDecisionStrip, DressingRoomHierarchy, ManagerPortal } from "./fm-surfaces";
+export { FmSpecialView } from "./fm-v6-special";
